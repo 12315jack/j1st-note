b140568fe94088a1c2eae827af8bb0c1ebe102c6
12315jack <chenyj_work@163.com>
2019-05-06 20:02:48 +0800

    add java basic docs

--- java-basic/src/main/java/www/j1stiot/cn/thread/resources/JoinDemo.java
+++ java-basic/src/main/java/www/j1stiot/cn/thread/resources/JoinDemo.java
@@ -1,5 +1,9 @@
 package www.j1stiot.cn.thread.resources;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
 /**
  *
  * join()方法测试
@@ -12,5 +16,41 @@ public class JoinDemo implements Runnable{
         for(int i=0;i<5;i++){
             System.out.println("线程1第: "+i+" 次执行");
         }
+    }
+
+    public static void main(String[] args) {
+        Lock lock = new Lock() {
+            @Override
+            public void lock() {
+
+            }
+
+            @Override
+            public void lockInterruptibly() throws InterruptedException {
+
+            }
+
+            @Override
+            public boolean tryLock() {
+                return false;
+            }
+
+            @Override
+            public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+                return false;
+            }
+
+            @Override
+            public void unlock() {
+
+            }
+
+            @Override
+            public Condition newCondition() {
+                return null;
+            }
+        };
+
+
     }
 }
